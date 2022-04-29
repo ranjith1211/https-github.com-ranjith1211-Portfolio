2bfc2d25b383d20b1c853656f173f1f5fd759ba5
ranjith1211 <97530933+ranjith1211@users.noreply.github.com>
2022-04-29 07:03:27 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <br></br>
+            <hr></hr>
+            <p className="footer-text">Made with ♥️ by Ranjith<br></br>
+            <br></br>
+            <small>All rights reserved © 2022 Ranjith</small><br></br>
+            <small>Illustrations by undraw.co</small>
+            </p>
+        </div>
+    )
+}
+
+export default Footer
